ec1bfb1ada94459cab7a58be5c97f23d50816e1a
wuzhuobin <jiejin2022@163.com>
2023-06-14 14:18:53 +0000

    Enable forward broadcast

--- gateway-setup-siit.sh
+++ gateway-setup-siit.sh
@@ -4,6 +4,13 @@
 sysctl -w net.ipv4.conf.all.forwarding=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 
+# enable ping broadcast response
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
+# enable broadcast forwarding
+sysctl -w net.ipv4.conf.all.bc_forwarding=1
+sysctl -w net.ipv4.conf.eth0.bc_forwarding=1
+sysctl -w net.ipv4.conf.eth1.bc_forwarding=1
+
 # setup the siit nat64 prefix for mapping IPv4 and IPv6 addresses.
 # This should be a common knowledge for all devices in the system.
 jool_siit instance add --netfilter --pool6 fd00::/96
